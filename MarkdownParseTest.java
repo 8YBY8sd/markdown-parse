4f9ccb3c43583a5f7f2255d8be59ae99829f7d68
8YBY8sd <b9yu@ucsd.edu>
2022-04-21 16:44:31 -0700

    lab4 part3
    used to run the tests
    assertEquals(2, 1 + 1);

---
+++ MarkdownParseTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;  //using jar file
+import org.junit.*; //using jar file
+public class MarkdownParseTest { //create a class
+    @Test //build test 
+    public void addition() { //addition method
+        assertEquals(2, 1 + 1); //check the output
+    }
+}
