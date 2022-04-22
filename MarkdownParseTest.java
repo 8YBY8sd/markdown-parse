f4fae4c3902d872640b16852ccfef80b1b943c9e
8YBY8sd <b9yu@ucsd.edu>
2022-04-21 19:45:31 -0700

    lab4 part4
    add another test method
    using Files.readString Path.of

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,25 @@
 import static org.junit.Assert.*;  //using jar file
 import org.junit.*; //using jar file
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MarkdownParseTest { //create a class
+
     @Test //build test 
     public void addition() { //addition method
         assertEquals(2, 1 + 1); //check the output
+    }
+    
+    @Test
+    public void getLinks() throws IOException {
+        List<String> list = List.of("https://something.com", "some-thing.html");
+        Path fileName = Path.of("C:\\Users\\yubin\\OneDrive - UC San Diego\\UCSD\\UCSD study\\CSE 15L\\lab\\lab3\\markdown-parser\\test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        
+        assertEquals(links, list);
     }
 }
