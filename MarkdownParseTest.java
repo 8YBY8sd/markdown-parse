96b322d1be8bec960613a36e99eb5e1898d9874d
8YBY8sd <b9yu@ucsd.edu>
2022-04-23 16:58:13 -0700

    lab4 part6
    set up JUnit tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -19,7 +19,7 @@ public class MarkdownParseTest { //create a class
         Path fileName = Path.of("C:\\Users\\yubin\\OneDrive - UC San Diego\\UCSD\\UCSD study\\CSE 15L\\lab\\lab3\\markdown-parser\\test-file.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
-        assertEquals(links, list);
+        assertEquals(list, links);
     }
 
     @Test
@@ -28,8 +28,69 @@ public class MarkdownParseTest { //create a class
         Path fileName = Path.of("C:\\Users\\yubin\\OneDrive - UC San Diego\\UCSD\\UCSD study\\CSE 15L\\lab\\lab3\\markdown-parser\\part3.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
-        assertEquals(links, list);
+        assertEquals(list, links);
     }
 
+    @Test
+    public void getLinksTestFile2() throws IOException {
+        List<String> list = List.of("https://something.com", "some-page.html");
+        Path fileName = Path.of("C:\\Users\\yubin\\OneDrive - UC San Diego\\UCSD\\UCSD study\\CSE 15L\\lab\\lab3\\markdown-parser\\test-file2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(list, links);
+    }
     
+    @Test
+    public void getLinksTestFile3() throws IOException {
+        List<String> list = List.of();
+        Path fileName = Path.of("C:\\Users\\yubin\\OneDrive - UC San Diego\\UCSD\\UCSD study\\CSE 15L\\lab\\lab3\\markdown-parser\\test-file3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(list, links);
+    }
+
+    @Test
+    public void getLinksTestFile4() throws IOException {
+        List<String> list = List.of();
+        Path fileName = Path.of("C:\\Users\\yubin\\OneDrive - UC San Diego\\UCSD\\UCSD study\\CSE 15L\\lab\\lab3\\markdown-parser\\test-file4.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(list, links);
+    }
+
+    @Test
+    public void getLinksTestFile5() throws IOException {
+        List<String> list = List.of();
+        Path fileName = Path.of("C:\\Users\\yubin\\OneDrive - UC San Diego\\UCSD\\UCSD study\\CSE 15L\\lab\\lab3\\markdown-parser\\test-file5.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(list, links);
+    }
+
+    @Test
+    public void getLinksTestFile6() throws IOException {
+        List<String> list = List.of();
+        Path fileName = Path.of("C:\\Users\\yubin\\OneDrive - UC San Diego\\UCSD\\UCSD study\\CSE 15L\\lab\\lab3\\markdown-parser\\test-file6.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(list, links);
+    }
+
+    @Test
+    public void getLinksTestFile7() throws IOException {
+        List<String> list = List.of();
+        Path fileName = Path.of("C:\\Users\\yubin\\OneDrive - UC San Diego\\UCSD\\UCSD study\\CSE 15L\\lab\\lab3\\markdown-parser\\test-file7.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(list, links);
+    }
+
+    @Test
+    public void getLinksTestFile8() throws IOException {
+        List<String> list = List.of("a link on the first line");
+        Path fileName = Path.of("C:\\Users\\yubin\\OneDrive - UC San Diego\\UCSD\\UCSD study\\CSE 15L\\lab\\lab3\\markdown-parser\\test-file8.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(list, links);
+    }
 }
