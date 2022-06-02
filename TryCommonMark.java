63f15b09d119721dd8b53a9e66e09a3eeb34f901
8YBY8sd <b9yu@ucsd.edu>
2022-06-02 16:57:00 -0700

    Update TryCommonMark.java

--- TryCommonMark.java
+++ TryCommonMark.java
@@ -6,8 +6,7 @@ public static void main(String[] args) {
     Parser parser = Parser.builder().build();
     Node document = parser.parse("This is *Sparta*");
     HtmlRenderer renderer = HtmlRenderer.builder().build();
-    renderer.render(document); // "<p>This is 
-    <em>Sparta</em></p>\n"
+    renderer.render(document); // "<p>This is <em>Sparta</em></p>\n"
     }
 }
 
